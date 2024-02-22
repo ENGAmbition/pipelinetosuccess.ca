6e8d1e26839b19cb3770ee815dba7a4819507327
tristan <heytristaann@gmail.com>
2024-02-22 16:24:12 -0500

    chore: add section stubs

---
+++ src/components/sections/Hero/index.tsx
@@ -0,0 +1,7 @@
+import { FC, JSX } from "react";
+
+const HeroSection: FC = (): JSX.Element => {
+  return <section></section>;
+};
+
+export default HeroSection;
---
+++ src/components/sections/Updates/index.tsx
@@ -0,0 +1,7 @@
+import { FC, JSX } from "react";
+
+const UpdatesSection: FC = (): JSX.Element => {
+  return <section></section>;
+};
+
+export default UpdatesSection;
