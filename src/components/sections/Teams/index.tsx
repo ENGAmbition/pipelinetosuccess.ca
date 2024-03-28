b98569ec1181c131d39c152b60378615ca9d54e1
Tristan Simpson <heytristaann@gmail.com>
2024-03-28 15:36:35 -0400

    chore: clean up

--- src/components/sections/Teams/index.tsx
+++ src/components/sections/Teams/index.tsx
@@ -1,54 +1,62 @@
-import Card from "pipeline-components";
-import { BrainIcon, DNAIcon, PeerHelpIcon } from "pipeline-components";
-import { GradientText } from "pipeline-components";
+import { Card, BrainIcon, DNAIcon, Button } from "pipeline-components";
+import { GradientText, PeerHelpIcon } from "pipeline-components";
 import { FC, JSX } from "react";
 
 const TeamsSection: FC = (): JSX.Element => {
   return (
-  <section>
-    <div className="z-0 flex flex-col items-center py-20 text-brand-neutral-600">
-      <div className="mt-1 text-4xl font-bold text-black">
-        Join Our Wonderful <GradientText>Team!</GradientText>
+    <section className="relative z-0 flex w-screen flex-col items-center bg-white p-12 py-20 text-brand-neutral-600">
+      <div className="z-0 flex flex-col items-center justify-center pb-12 text-brand-neutral-600">
+        <div className="mt-1 text-center text-4xl font-bold text-black">
+          Join Our Wonderful <GradientText>Team!</GradientText>
+        </div>
+        <p className="mt-4 w-full max-w-[40rem] text-center">
+          Seeking motivated individuals for diverse roles in our team. Open to
+          students, professionals, and volunteers eager to contribute to our
+          success. Explore opportunities and embrace challenges.
+        </p>
       </div>
-      <p className="mt-4 max-w-[40rem] text-center">
-        Seeking motivated individuals for diverse roles in our team. Open to students,
-        professionals, and volunteers eager to contribute to our success. Explore
-        opportunities and embrace challenges.
-      </p>
-    </div>
-    <div className="flex flex-wrap items-center justify-center gap-7">
-      <Card
-        title="Web/Graphics Designer"
-        content="Join us if you love crafting visually
+
+      <div className="flex flex-wrap items-center justify-center gap-7">
+        <Card
+          title="Web/Graphics Designer"
+          content="Join us if you love crafting visually
                  stunning websites and graphics.
                  Use your creative flair to design
                  captivating digital experiences that
                  leave a lasting impression."
-        icon={<PeerHelpIcon/>}
-        buttonText="Apply Now"
-      />
-      <Card
-        title="Social Media"
-        content="Be the voice of our brand across
+          icon={<PeerHelpIcon />}
+          button={
+            <Button className="rounded-full font-poppins">Apply Now</Button>
+          }
+        />
+        <Card
+          title="Social Media"
+          content="Be the voice of our brand across
                  social platforms! Join our team to
                  create engaging content, interact
                  with our audience, and drive
                  meaningful connections with our
                  community."
-        icon={<DNAIcon/>}
-        buttonText="Apply Now"
-      />
-      <Card
-      title="Academics"
-      content="Dive into the world of knowledge
+          icon={<DNAIcon />}
+          button={
+            <Button className="rounded-full font-poppins">Apply Now</Button>
+          }
+        />
+        <Card
+          title="Academics"
+          content="Dive into the world of knowledge
                with us! you'll contribute your
                expertise in physics, chemistry,
-               math, or biochemistry."
-      icon={<BrainIcon/>}
-      buttonText="Apply Now"
-    />
-    </div>
-  </section>
+               math, or biochemistry. Help us
+                develop engaging academic
+                resources and workshops."
+          icon={<BrainIcon />}
+          button={
+            <Button className="rounded-full font-poppins">Apply Now</Button>
+          }
+        />
+      </div>
+    </section>
   );
 };
 
